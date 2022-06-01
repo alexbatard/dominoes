9588c3f5e7d8966a5f2da5fbc5328f022092165d
Alex Batard <51515265+alexbatard@users.noreply.github.com>
2022-06-01 23:25:48 +0200

    commit

--- tests.py
+++ tests.py
@@ -3,41 +3,60 @@ import pygame
 from dominoes.board import Board
 from dominoes.half_domino import HalfDomino
 from dominoes.game import Game
-from dominoes.constants import WIDTH, HEIGHT, SQUARE_SIZE
+from gui.button import Button
+from dominoes.constants import WIDTH, HEIGHT, SQUARE_SIZE, PLAYER1,\
+    BLACK, WHITE
+from gui.constants import SMALL_FONT, BUTTON
 
 
 class Tests(unittest.TestCase):
-    def test_init(self):
-        board = Board(2, 7)
-        half_domino = HalfDomino(0, 1, 5)
+
+    def testInit(self):
+        board = Board()
+        half_domino = HalfDomino(0, 1, 5, PLAYER1)
         game = Game(pygame.display.set_mode((WIDTH, HEIGHT)))
-        self.assertEqual(board.nb_players, 2)
-        self.assertEqual(board.nb_dominoes_per_player, 7)
+        button = Button(pos=(WIDTH / 2, HEIGHT / 2),
+                        font=SMALL_FONT,
+                        image=BUTTON,
+                        base_color=BLACK,
+                        hovering_color=WHITE,
+                        text_input="Button")
+        self.assertEqual(board.dominoes_per_player, 7)
         self.assertEqual(half_domino.row, 0)
         self.assertEqual(half_domino.col, 1)
         self.assertEqual(half_domino.value, 5)
+        self.assertEqual(half_domino.player, PLAYER1)
         self.assertEqual(game.win, pygame.display.set_mode((WIDTH, HEIGHT)))
+        self.assertEqual(button.x, WIDTH / 2)
+        self.assertEqual(button.y, HEIGHT / 2)
 
-    def test_type(self):
-        board = Board(2, 7)
-        half_domino = HalfDomino(0, 1, 5)
+    def testType(self):
+        board = Board()
+        half_domino = HalfDomino(0, 1, 5, PLAYER1)
         game = Game(pygame.display.set_mode((WIDTH, HEIGHT)))
+        button = Button(pos=(WIDTH / 2, HEIGHT / 2),
+                        font=SMALL_FONT,
+                        image=BUTTON,
+                        base_color=BLACK,
+                        hovering_color=WHITE,
+                        text_input="Button")
         self.assertEqual(type(board), Board)
         self.assertEqual(type(half_domino), HalfDomino)
         self.assertEqual(type(game), Game)
+        self.assertEqual(type(button), Button)
 
-    def test_fill_domino_values(self):
-        board = Board(2, 7)
+    def testFillDominoValues(self):
+        board = Board()
         board.fillDominoValues()
         self.assertEqual(len(board.domino_values), 28)
 
-    def test_create_board(self):
-        board = Board(2, 7)
-        self.assertEqual(len(board.board), 26)
-        self.assertEqual(len(board.board[0]), 20)
+    def testCreateBoard(self):
+        board = Board()
+        self.assertEqual(len(board), 21)
+        self.assertEqual(len(board[0]), 15)
 
-    def test_calc_pos(self):
-        half_domino = HalfDomino(0, 1, 5)
+    def testCalcPos(self):
+        half_domino = HalfDomino(0, 1, 5, PLAYER1)
         self.assertEqual(half_domino.x, SQUARE_SIZE + SQUARE_SIZE // 2)
         self.assertEqual(half_domino.y, SQUARE_SIZE // 2)
 
